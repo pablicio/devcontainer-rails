c3fabc0f5c3b15895790a246bcf45f9ac37fc91a
pablicio <pabliciotjg@gmail.com>
2023-11-18 19:08:17 -0300

    Add first files

---
+++ .devcontainer/boot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Install dependencies"
+
+echo "Updating RubyGems..."
+gem install bundler -v 2.4.6
+rvm install 2.7.8
+
+echo "Installing dependencies..."
+bundle install
+
+echo "Creating database..."
+rails db:setup
+
+echo "Done!"
+
+# OBS MUDAR
+# POSTGRESQL_USER='postgres'
+# POSTGRESQL_HOST=postgres
+# POSTGRESQL_PASSWORD='postgres'
+
+# MESSAGE_BROKER_URL='amqp://guest:guest@rabbitmq:5672'
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# devcontainer-rails
-Workspace As Code With vscode devcontainer
+### Tutorial
+
+- Learn: https://cadu.dev/using-devcontainers-to-setup-your-dev-environment/
+- Based from: https://github.com/luizkowalski/devcontainer-rails/
